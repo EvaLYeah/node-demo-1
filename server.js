cf1881a50b1d76fc1ac644964a458f27af7effc5
lyhhh <17610603221@163.com>
2020-07-07 00:15:51 +0800

    修改文字提示内容

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var url = require('url')
 var port = process.argv[2]
 
 if(!port){
-  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  console.log('请指定端口\n比如这样：node server.js 8888')
   process.exit(1)
 }
 
@@ -19,12 +19,12 @@ var server = http.createServer(function(request, response){
 
   /******** 从这里开始看，上面不要看 ************/
 
-  console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+  console.log('有人发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
 
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`二哈`)
+    response.write(`耶耶耶`)
     response.end()
   } else if(path === '/x'){
     response.statusCode = 200
@@ -42,4 +42,4 @@ var server = http.createServer(function(request, response){
 })
 
 server.listen(port)
-console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+console.log('监听 ' + port + ' 成功\n点击打开：http://localhost:' + port)
